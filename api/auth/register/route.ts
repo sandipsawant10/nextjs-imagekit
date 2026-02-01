5d409a3f298b8ab63a3307fbba6935fb020d74d9
sandipsawant10 <sandipsawant503@gmail.com>
2026-02-01 22:21:08 +0530

    feat: implement authentication and user registration with NextAuth

---
+++ api/auth/register/route.ts
@@ -0,0 +1,42 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 },
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exist" },
+        { status: 400 },
+      );
+    }
+
+    await User.create({
+      email,
+      password,
+    });
+
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Registration error", error);
+    return NextResponse.json(
+      { message: "failed to register user" },
+      { status: 200 },
+    );
+  }
+}
